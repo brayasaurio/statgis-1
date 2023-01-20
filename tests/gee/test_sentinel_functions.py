cf8bea426db4a4109a3abeaad0a7ae477b03055f
Sebástian Narváez <65937357+srnarvaez@users.noreply.github.com>
2023-01-19 19:35:11 -0500

    test: test sentinel_functions

---
+++ tests/gee/test_sentinel_functions.py
@@ -0,0 +1,23 @@
+"""Test functions from gee.sentinel_functions submodule"""
+
+import ee
+from statgis.gee.sentinel_functions import cloud_mask, scaler
+
+ee.Initialize()
+
+def test_sentinel_function():
+    """Test scaler and cloud_mask functions"""
+
+    coords = [-70.025, -55.409, -65.487, -53.400]
+
+    roi = ee.Geometry.BBox(*coords)
+    sentinel_2 = (
+        ee.ImageCollection("COPERNICUS/S2_SR")
+          .filterBounds(roi)
+          .map(cloud_mask)
+          .map(scaler)
+    )
+
+    img = sentinel_2.mean()
+
+    assert isinstance(img, ee.Image)
